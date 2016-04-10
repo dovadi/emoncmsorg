483ea6ca2538524a089295ce577b055a9051540d
TrystanLea <glyn.hudson@openenergymonitor.org>
2016-04-10 16:42:57 +0100

    improved interval rounding to match graph module approach

--- emoncms/Modules/vis/visualisations/multigraph.js
+++ emoncms/Modules/vis/visualisations/multigraph.js
@@ -87,10 +87,7 @@
       }
       if (plotlist[i].selected) {
         if (!plotlist[i].plot.data)
-        {
-          var npoints = 800;
-          interval = Math.round(((view.end - view.start)/npoints)/1000);
-          
+        {          
           var skipmissing = 0;
           
           if (multigraph_feedlist[i]['skipmissing']==undefined) {
@@ -98,6 +95,16 @@
           } else {
               if (multigraph_feedlist[i]['skipmissing']) skipmissing = 1;
           }
+
+          var npoints = 800;
+          interval = Math.round(((view.end - view.start)/npoints)/1000);
+          
+          // Round to more common and useful intervals:
+          interval = view.round_interval(interval);
+           
+          var intervalms = interval*1000;
+          view.start = Math.round(view.start/intervalms)*intervalms;
+          view.end = Math.round(view.end/intervalms)*intervalms;
           
           plotlist[i].plot.data = get_feed_data(plotlist[i].id,view.start,view.end,interval,skipmissing,1);
         }
